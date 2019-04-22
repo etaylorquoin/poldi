e89faa7709b12ee1979483e736214e5a6d8387f0
Eric Taylor <etaylor@quoininc.com>
2019-04-22 10:01:48 -0400

    Working on Starting gpg-agent

--- src/scd/scd.c
+++ src/scd/scd.c
@@ -245,6 +245,14 @@ scd_connect (scd_context_t *scd_ctx, int use_agent, const char *scd_path,
 
   log_msg_error (loghandle, "AT if user-agnet==2");
 
+  if (fflush (NULL))
+      {
+        err = gpg_error_from_syserror ();
+        log_msg_error (loghandle, "error flushing pending output: %s",
+  		     strerror (errno));
+        return err;
+      }
+
   /* Try using scdaemon under gpg-agent. under user */
   if (use_agent == 2)
   {
