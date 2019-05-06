df20f4223eff50b6c8e0ead87f067709ac80c39f
Eric Taylor <etaylor@quoininc.com>
2019-05-06 14:43:25 -0400

    add Options

--- tests/session-test.c
+++ tests/session-test.c
@@ -106,7 +106,7 @@ int main (int argc, const char *argv[])
 
 	username = argv[optind];
 
-	printf("Testing for user %s \n", argv[1]);
+	printf("Testing for user %s \n", username);
 	//printf("Testing scd_connect() \n");
 	//test_scd_connect(loghandle);
 
