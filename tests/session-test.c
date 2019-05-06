e0f6aaa4cf1b5b58f7e38b842bb91dabbcfa38d2
Eric Taylor <etaylor@quoininc.com>
2019-05-06 13:33:14 -0400

    adding session test

--- tests/session-test.c
+++ tests/session-test.c
@@ -1,6 +1,8 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <getopt.h>
 
 #include <gpg-error.h>
 
@@ -8,7 +10,11 @@
 #include <simplelog.h>
 #include "scd/scd.h"
 
+#define PROGRAM_NAME    "pam-session"
+#define PROGRAM_VERSION "0.1"
+
 //void test_scd_connect(const char *username, log_handle_t loghandle, pam_handle_t *pam_handle);
+static void print_help(void);
 
 int main (int argc, const char **argv)
 {
@@ -16,6 +22,7 @@ int main (int argc, const char **argv)
 	log_handle_t loghandle = NULL;
 	simpleparse_handle_t handle = NULL;
 	pam_handle_t *pam_handle = NULL;
+	int rt_val;
 
 	assert (argc > 0);
 
@@ -27,12 +34,31 @@ int main (int argc, const char **argv)
 
 	if (argc != 2)
 	{
-		printf("Error please provide username as argument\n");
+		print_help();
 		return 1;
 	}
 	else
 	{
 		printf("Testing for user %s \n", argv[1]);
+	}
+
+	//while(1)
+	{
+		static struct option long_options[] =
+		{
+		  { "version", no_argument, 0, 'v' },
+		  { "help", no_argument, 0, 'h' },
+		  { 0, 0, 0, 0 }
+		};
+
+		int index = 0;
+
+		rt_val = getopt_long (argc, argv, "vh:",
+			       long_options, &index);
+
+		printf("rt_val: %d", rt_val);
+
+
 	}
 
 	//printf("Testing scd_connect() \n");
@@ -41,6 +67,19 @@ int main (int argc, const char **argv)
 	return 0;
 
 }//main
+
+
+static void print_help (void)
+{
+  printf ("\
+Usage: %s [options] <PAM user name>\n\
+Test PAM session.\n\
+\n\
+Options:\n\
+ -h, --help      print help information\n\
+ -v, --version   print version information\n\
+\n", PROGRAM_NAME);
+}
 
 //void test_scd_connect(const char *username, log_handle_t loghandle, pam_handle_t *pam_handle)
 //{
