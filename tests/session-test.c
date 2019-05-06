534d1f5fbe6bd9fb5b02f66145869c6566134025
Eric Taylor <etaylor@quoininc.com>
2019-05-06 12:09:45 -0400

    adding session test

---
+++ tests/session-test.c
@@ -0,0 +1,70 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+#include <gpg-error.h>
+
+#include <simpleparse.h>
+#include <simplelog.h>
+#include "scd/scd.h"
+
+//void test_scd_connect(const char *username, log_handle_t loghandle, pam_handle_t *pam_handle);
+
+int main (int argc, const char **argv)
+{
+	gpg_error_t err = 0;
+	log_handle_t loghandle = NULL;
+	simpleparse_handle_t handle = NULL;
+	pam_handle_t *pam_handle = NULL;
+
+	assert (argc > 0);
+
+	err = log_create (&loghandle);
+	assert (!err);
+
+	err = log_set_backend_stream (loghandle, stderr);
+	assert (!err);
+
+	if (argc != 2)
+	{
+		printf("Error please provide username as argument\n");
+		return 1;
+	}
+	else
+	{
+		printf("Testing for user %s \n", argv[1]);
+	}
+
+	//printf("Testing scd_connect() \n");
+	//test_scd_connect(loghandle);
+
+	return 0;
+
+}//main
+
+//void test_scd_connect(const char *username, log_handle_t loghandle, pam_handle_t *pam_handle)
+//{
+//	gpg_error_t err = 0;
+//
+//	err = log_set_backend_stream (loghandle, stderr);
+//	assert (!err);
+//
+//	gpg_error_t err;
+//	scd_context_t scd_ctx;
+//	int use_agent = 2;
+//
+//	struct passwd pwd, *result;
+//	char *buf = NULL;
+//	size_t bufsize;
+//
+//	bufsize = sysconf(_SC_GETPW_R_SIZE_MAX);
+//	if (bufsize == -1)
+//	{
+//		bufsize = 16384;
+//	}
+//
+//	//allocate and get users passwd strcut
+//	//buf = (char*) malloc(bufsize);
+//	//ret = getpwnam_r(pam_username, &pwd, buf, bufsize, &result);
+//}
+
