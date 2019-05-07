95bb26c482f39bce704667943e14741cb07eae98
Eric Taylor <etaylor@quoininc.com>
2019-05-07 16:19:40 -0400

    add pam session test start script

--- tests/run-pam-session-test.sh
+++ tests/run-pam-session-test.sh
@@ -1,11 +1,12 @@
 #!/bin/sh 
 
 session_bin='session-test'
-preloader_cmd='LD_PRELOAD=libpam_wrapper.so PAM_WRAPPER=1 PAM_WRAPPER_USE_SYSLOG=0 PAM_WRAPPER_DEBUGLEVEL=3 PAM_WRAPPER_SERVICE_DIR=./services ./session-test'
+preloader_cmd='LD_PRELOAD=libpam_wrapper.so PAM_WRAPPER=1 PAM_WRAPPER_USE_SYSLOG=0 PAM_WRAPPER_DEBUGLEVEL=2 PAM_WRAPPER_SERVICE_DIR=./services ./session-test'
 
 if  [ -f "${session_bin}" ]
 then
-   eval "${preloader_cmd}"
+   printf '\nStarting libpam with preloader . . .\n\n'
+   eval "${preloader_cmd} $1"
 else
    printf 'Error Please compile session-test\n'
 fi
