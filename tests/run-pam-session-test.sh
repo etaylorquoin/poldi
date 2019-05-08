6f832d5c7dccbfbb416a9c5666a54f837b1882cf
Eric Taylor <etaylor@quoininc.com>
2019-05-08 17:19:07 -0400

    update test script

--- tests/run-pam-session-test.sh
+++ tests/run-pam-session-test.sh
@@ -5,13 +5,15 @@
 
 #bins
 pam_session_test_id='/usr/bin/id'
+pam_session_test_ls='/usr/bin/ls'
+
 session_bin='session-test'
 
 #preloader command with options
 preloader_cmd='LD_PRELOAD=libpam_wrapper.so PAM_WRAPPER=1 PAM_WRAPPER_USE_SYSLOG=0 PAM_WRAPPER_DEBUGLEVEL=2 PAM_WRAPPER_SERVICE_DIR=./services ./session-test'
 
 #paths
-pam_session_run_dir='/run/usr'
+pam_session_run_dir='/run/user/'
 pam_session_gpg_sock='/gnupg/S.gpg-agent'
 pam_session_scd_sock='/gnupg/S.scdaemon'
 
@@ -21,17 +23,17 @@ then
    if [ ! -z "$1" ]
    then
       pam_session_uid=$(eval "${pam_session_test_id} --user $1")
-      
-      if [ -f "${pam_session_run_dir}${pam_session_uid}${pam_session_gpg_sock}" ]
+
+      if eval "${pam_session_test_ls} ${pam_session_run_dir}${pam_session_uid}${pam_session_gpg_sock}" 2> '/dev/null'
       then
          printf 'Warning Found Running gpg-agent for user\n'
-         printf 'Please stop gpg-agent before testing\n'
+         printf 'Please stop gpg-agent before testing\n\n'
       fi
       
-      if [ -f "${pam_session_run_dir}${pam_session_uid}${pam_session_scd_sock}" ]
+      if eval "${pam_session_test_ls} ${pam_session_run_dir}${pam_session_uid}${pam_session_scd_sock}" 2> '/dev/null'
       then
          printf 'Warning Found Running scdaemon for user\n'
-         printf 'Please stop scdaemon before testing\n'
+         printf 'Please stop scdaemon before testing\n\n'
       fi
       
    fi
