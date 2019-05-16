8a7996d0b4ce9db2c239eac5216adadf7d4f6255
Eric Taylor <etaylor@quoininc.com>
2019-05-16 15:02:53 -0400

    init

--- src/scd/scd.c
+++ src/scd/scd.c
@@ -1,19 +1,19 @@
 /* scd.c - Interface to Scdaemon
    Copyright (C) 2001, 2002, 2005 Free Software Foundation, Inc.
-   Copyright (C) 2007, 2008, 2009 g10code GmbH.
+   Copyright (C) 2007, 2008, 2009 g10code GmbH. 
 
    This file is part of Poldi.
-
+ 
    Poldi is free software; you can redistribute it and/or modify it
    under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-
+ 
    Poldi is distributed in the hope that it will be useful, but
    WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    General Public License for more details.
-
+ 
    You should have received a copy of the GNU General Public License
    along with this program; if not, see
    <http://www.gnu.org/licenses/>.  */
@@ -50,7 +50,7 @@
 #define MAX_OPEN_FDS 20
 #endif
 
-#define READ_END 0
+#define READ_END 0
 #define WRITE_END 1
 
 #define TRUE 1
@@ -81,7 +81,7 @@ struct learn_parm_s
   void *sinfo_cb_arg;
 };
 
-struct inq_needpin_s
+struct inq_needpin_s 
 {
   scd_context_t ctx;
   int (*getpin_cb)(void *, const char *, char*, size_t);
@@ -118,7 +118,7 @@ get_agent_socket_name (char **gpg_agent_sockname)
 
   /* It is good if we have popen with execv (no SHELL) */
   input = popen (GNUPG_DEFAULT_GPGCONF " --list-dirs agent-socket", "r");
-  if (input == (void*) NULL)
+  if (input == NULL)
     {
       xfree (result);
       return gpg_error (GPG_ERR_NOT_FOUND);
@@ -130,45 +130,6 @@ get_agent_socket_name (char **gpg_agent_sockname)
   if (len)
     {
       *gpg_agent_sockname = result;
-      result[len-1] = 0;	/* Chop off the newline.  */
-    }
-  else
-    {
-      xfree (result);
-      err =  gpg_error (GPG_ERR_NOT_FOUND);
-    }
-
-  return err;
-}
-
-/* Get the bindir of GPG-AGENT by gpgconf. */
-static gpg_error_t get_agent_bin_dir (char **gpg_agent_bindir)
-{
-  gpg_error_t err = 0;
-  FILE *input;
-  char *result;
-  size_t len;
-
-  *gpg_agent_bindir = NULL;
-
-  result = xtrymalloc (256);
-  if (!result)
-    return gpg_error_from_syserror ();
-
-  /* It is good if we have popen with execv (no SHELL) */
-  input = popen (GNUPG_DEFAULT_GPGCONF " --list-dirs bindir", "r");
-  if (input == (void*) NULL)
-    {
-      xfree (result);
-      return gpg_error (GPG_ERR_NOT_FOUND);
-    }
-
-  len = fread (result, 1, 256, input);
-  fclose (input);
-
-  if (len)
-    {
-      *gpg_agent_bindir = result;
       result[len-1] = 0;	/* Chop off the newline.  */
     }
   else
@@ -560,7 +521,7 @@ unescape_status_string (const char *s)
   while (*s)
     {
       if (*s == '%' && s[1] && s[2])
-        {
+        { 
           s++;
           *d = xtoi_2 (s);
           if (!*d)
@@ -576,7 +537,7 @@ unescape_status_string (const char *s)
       else
         *d++ = *s++;
     }
-  *d = 0;
+  *d = 0; 
   return buffer;
 }
 
@@ -675,7 +636,7 @@ learn_status_cb (void *opaque, const char *line)
       else if (no == 3)
         parm->fpr3valid = unhexify_fpr (line, parm->fpr3);
     }
-
+  
   return 0;
 }
 
@@ -790,7 +751,7 @@ membuf_data_cb (void *opaque, const void *buffer, size_t length)
     put_membuf (data, buffer, length);
   return 0;
 }
-
+  
 /* Handle the NEEDPIN inquiry. */
 static int
 inq_needpin (void *opaque, const char *line)
@@ -813,7 +774,7 @@ inq_needpin (void *opaque, const char *line)
       line += 7;
       while (*line == ' ')
         line++;
-
+      
       pinlen = 90;
       pin = xtrymalloc_secure (pinlen);
       if (!pin)
@@ -839,7 +800,7 @@ inq_needpin (void *opaque, const char *line)
       line += 17;
       while (*line == ' ')
         line++;
-
+      
       rc = parm->getpin_cb (parm->getpin_cb_arg, line, NULL, 1);
     }
   else if (!strncmp (line, "DISMISSPINPADPROMPT", 19)
@@ -873,12 +834,11 @@ inq_needpin (void *opaque, const char *line)
    the PIN, GETPIN_CB_ARG is passed as opaque argument to
    GETPIN_CB. INDATA/INDATALEN is the input for the signature
    function.  The signature created is written into newly allocated
-   memory in *R_BUF, *S_BUF, *R_BUFLEN, S_BUFLEN will hold the length of the
+   memory in *R_BUF, *R_BUFLEN will hold the length of the
    signature. */
 gpg_error_t
 scd_pksign (scd_context_t ctx,
 	    const char *keyid,
-      key_types key_type,
 	    const unsigned char *indata, size_t indatalen,
 	    unsigned char **r_buf, size_t *r_buflen)
 {
@@ -889,7 +849,6 @@ scd_pksign (scd_context_t ctx,
   size_t len;
   unsigned char *sigbuf;
   size_t sigbuflen;
-  const char * hashAlgo;
 
   *r_buf = NULL;
   *r_buflen = 0;
@@ -922,25 +881,9 @@ scd_pksign (scd_context_t ctx,
   inqparm.getpin_cb = ctx->pincb;
   inqparm.getpin_cb_arg = ctx->pincb_cookie;
 
-
-  //set hash type based on
-  switch (key_type)
-  {
-    case kType_rsa:
-      hashAlgo = "--hash=sha256";
-      break;
-
-    case kType_ecc_Ed25519:
-      hashAlgo = "";//default is sha512, setting that here causes scd to generate an invalid SIG
-      break;
-
-    default:
-      rc = -1;
-      goto out;
-  }//switch
-
   /* Go, sign it. */
-  snprintf (line, DIM(line)-1, "PKSIGN %s %s",hashAlgo, keyid);
+
+  snprintf (line, DIM(line)-1, "PKSIGN %s", keyid);
   line[DIM(line)-1] = 0;
   rc = assuan_transact (ctx->assuan_ctx, line,
                         membuf_data_cb, &data,
@@ -963,7 +906,7 @@ scd_pksign (scd_context_t ctx,
     }
 
   memcpy (p, sigbuf, sigbuflen);
-
+  
  out:
 
   xfree (get_membuf (&data, &len));
@@ -1019,7 +962,7 @@ scd_readkey (scd_context_t ctx,
  out:
 
   xfree (buffer);
-
+    
   return rc;
 }
 
